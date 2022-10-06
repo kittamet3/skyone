489c25f254d4c247c27feefe76f653b6c0c6c2c2
kittamet3 <114392629+kittamet3@users.noreply.github.com>
2022-10-06 16:28:03 +0700

    Skyone3

--- main.ts
+++ main.ts
@@ -22,6 +22,38 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(200)
     }
 })
+function Line_follow () {
+    Loop = true
+    ค่าแสงP0 = 440
+    ค่าแสงP1 = 464
+    ค่าแสงP2 = 626
+    ค่าแสงP3 = 728
+    while (Loop) {
+        if (MySensor.analogRead(MySensor.analogPort.P0) > ค่าแสงP0 && MySensor.analogRead(MySensor.analogPort.P1) > ค่าแสงP1 && (MySensor.analogRead(MySensor.analogPort.P2) > ค่าแสงP2 && MySensor.analogRead(MySensor.analogPort.P3) > ค่าแสงP3)) {
+            MyRobotBit.MotorAB(motorDIR.Forward, 50, 50)
+        } else {
+            if (MySensor.analogRead(MySensor.analogPort.P0) < ค่าแสงP0 && MySensor.analogRead(MySensor.analogPort.P1) > ค่าแสงP1 && (MySensor.analogRead(MySensor.analogPort.P2) > ค่าแสงP2 && MySensor.analogRead(MySensor.analogPort.P3) > ค่าแสงP3)) {
+                MyRobotBit.Rotate(Turn.Left, 50, 200)
+                MyRobotBit.TimePAUSE(100)
+                MyRobotBit.MotorAB(motorDIR.Reverse, 50, 50)
+                MyRobotBit.TimePAUSE(100)
+                MyRobotBit.MotorAB(motorDIR.Forward, 50, 50)
+            } else if (MySensor.analogRead(MySensor.analogPort.P0) > ค่าแสงP0 && MySensor.analogRead(MySensor.analogPort.P1) < ค่าแสงP1 && (MySensor.analogRead(MySensor.analogPort.P2) > ค่าแสงP2 && MySensor.analogRead(MySensor.analogPort.P3) > ค่าแสงP3)) {
+                MyRobotBit.Rotate(Turn.Right, 50, 200)
+                MyRobotBit.TimePAUSE(100)
+                MyRobotBit.MotorAB(motorDIR.Reverse, 50, 50)
+                MyRobotBit.TimePAUSE(100)
+                MyRobotBit.MotorAB(motorDIR.Forward, 50, 50)
+            } else {
+                if (MySensor.analogRead(MySensor.analogPort.P0) < ค่าแสงP0 && MySensor.analogRead(MySensor.analogPort.P1) < ค่าแสงP1 && (MySensor.analogRead(MySensor.analogPort.P2) > ค่าแสงP2 && MySensor.analogRead(MySensor.analogPort.P3) > ค่าแสงP3)) {
+                    index = false
+                    MyRobotBit.motorOFF(motorSEL.M12, StopMode.Brake)
+                }
+            }
+        }
+    }
+    MyRobotBit.motorOFF(motorSEL.M12, StopMode.Brake)
+}
 input.onButtonPressed(Button.AB, function () {
     MyRobotBit.TimePAUSE(1200)
     go()
@@ -36,8 +68,14 @@ function Stop () {
     MyRobotBit.motorOFF(motorSEL.M12, StopMode.Brake)
 }
 function go () {
-	
+    Line_follow()
 }
+let index = false
+let ค่าแสงP3 = 0
+let ค่าแสงP2 = 0
+let ค่าแสงP1 = 0
+let ค่าแสงP0 = 0
+let Loop = false
 led.enable(false)
 OLED.init(128, 64)
 while (!(input.buttonIsPressed(Button.A))) {
